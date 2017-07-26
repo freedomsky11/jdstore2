7c4f2a1cba0354e0b502e1c993b887bfb4217358
freedomsky <hanwei_1989@126.com>
2017-07-26 11:28:47 +0800

    add flash&devise&simple_form function

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,18 @@
+class Admin::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Prodcut.find(params[:id])
+  end
+  
+end
