757791b191544a44e8aaad64828d429cd59a88ed
freedomsky <hanwei_1989@126.com>
2017-08-15 16:08:27 +0800

    empty cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,8 +9,12 @@ def index
 
   def add_to_cart
     @product = Product.find(params[:id])
-    current_cart.add_product_to_cart(@product)
-    flash[:notice] = "成功加入购物车"
+    if @product.quantity > 0
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "成功加入购物车"
+    else
+      flash[:alert] = "该商品目前无货。"
+    end
     redirect_to :back
   end
 
